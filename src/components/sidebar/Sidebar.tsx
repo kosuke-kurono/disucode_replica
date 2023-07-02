510ffd3d3ac7bc04d6a29f11804d8f0c5a192f5a
kosuke-kurono <kurono.nexus6@gmail.com>
2023-07-02 14:21:55 +0900

    サイドバー右アイコン追加

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import './Sidebar.scss';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       {/*サイドバー左*/}
       <div className="sidebarLeft">
-        <div className='serverIcon'>
-            <img src="./logo192.png" alt="" />
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
         </div>
-        <div className='serverIcon'>
-            <img src="./logo192.png" alt="" />
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
         </div>
       </div>
       {/*サイドバー右*/}
       <div className="sidebarRight">
-        <div className='sideberTop'>
-            <h3>Discord</h3>
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+        {/*サイドバーチャンネル*/}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>テキストチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
         </div>
       </div>
     </div>
